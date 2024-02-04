5edff6ce3f3235c7f1807ab73caa11c1b7ab6443
Sushi <158117453+nlam0@users.noreply.github.com>
2024-02-03 19:40:19 -0500

    testing

---
+++ src/pages/logger.tsx
@@ -0,0 +1,14 @@
+export default function Logger() {
+
+    return (
+     <>
+     <Head>
+        <title>Travel Log</title>
+        <meta name="description" content="Travel log of transit rides" />
+        <link rel="icon" href="/favicon.ico" />
+     </Head>
+     <main className></main>
+    
+    </>
+    );
+}
